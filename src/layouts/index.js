8079cbba183db7562c89c4b8874800f6fa22483d
Kylee Tilley <kyleetilley@gmail.com>
2018-08-04 00:11:25 -0500

    Strip out styling

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 
-import Header from '../components/header'
-import './index.css'
+import Header from '../components/header';
+import './index.css';
 
 const Layout = ({ children, data }) => (
   <div>
@@ -15,24 +15,15 @@ const Layout = ({ children, data }) => (
       ]}
     />
     <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+    <div>{children()}</div>
   </div>
-)
+);
 
 Layout.propTypes = {
   children: PropTypes.func,
-}
+};
 
-export default Layout
+export default Layout;
 
 export const query = graphql`
   query SiteTitleQuery {
@@ -42,4 +33,4 @@ export const query = graphql`
       }
     }
   }
-`
+`;
