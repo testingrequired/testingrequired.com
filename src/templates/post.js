734acd2b515cde8a5472f45f071d412793202e8f
Kylee Tilley <kyleetilley@gmail.com>
2019-07-28 12:21:40 -0500

    Refactor post template

--- src/templates/post.js
+++ src/templates/post.js
@@ -2,19 +2,20 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import Layout from '../layouts';
 export default function Template({ data }) {
-  const post = data.markdownRemark;
+  const { frontmatter, html } = data.markdownRemark;
+
   return (
     <Layout>
       <div className="blog-post-container">
-        <Helmet title={`${post.frontmatter.title} - Testing Required`} />
+        <Helmet title={`${frontmatter.title} - Testing Required`} />
         <div className="blog-post">
-          <h1>{post.frontmatter.title}</h1>
+          <h1>{frontmatter.title}</h1>
 
-          <p>{post.frontmatter.date}</p>
+          <p>{frontmatter.date}</p>
 
           <div
             className="blog-post-content"
-            dangerouslySetInnerHTML={{ __html: post.html }}
+            dangerouslySetInnerHTML={{ __html: html }}
           />
         </div>
       </div>
