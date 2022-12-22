79e875bec5bf7562eda47238141929f429ccf681
Kylee Tilley <kyleetilley@gmail.com>
2022-12-22 01:17:05 -0600

    Adding more talks

--- src/pages/talks.js
+++ src/pages/talks.js
@@ -10,13 +10,28 @@ export default function Talks() {
       <ul>
         <li>
           <OutboundLink href="https://www.ministryoftesting.com/dojo/series/test-bash-2022/lessons/transcending-the-tools">
-            {`Transcending The Tools @ Test.Bash() 2022`}
-          </OutboundLink>
+            {`Transcending The Tools`}
+          </OutboundLink> @ Test.Bash() 2022 <OutboundLink href="https://docs.google.com/presentation/d/1Po5RHzyJ--s7kDGY2jI_KxyqQJnNrvbREiF-35rCnvY/edit?usp=sharing">(slides)</OutboundLink>
+        </li>
+        <li>
+          <OutboundLink href="https://docs.google.com/presentation/d/1lWyIckm1lCITqxoRLYA0nLO3hKZ6hp11dtfbaKeHc7w/edit?usp=sharing">
+            {`Performance, Stress and Load Testing, Oh My!`}
+          </OutboundLink> 2020 (slides only)
         </li>
         <li>
           <OutboundLink href="https://devopsdays.org/events/2019-des-moines/program/kylee-tilley/">
-            {`QA: A Task Not A Role @ devopsdays dsm 2019`}
-          </OutboundLink>
+            {`QA: A Task Not A Role`}
+          </OutboundLink> @ devopsdays dsm 2019 <OutboundLink href="https://docs.google.com/presentation/d/1Zuvq4iNhYT9ousGXRe-TdJrx8hd7D0aQ0-XlhFVkji4/edit?usp=sharing">(slides)</OutboundLink>
+        </li>
+        <li>
+          <OutboundLink href="https://docs.google.com/presentation/d/1ntsjTF2R8SEI66FJFBjAaheN-6mfBsP_ZqgBsGM78HM/edit?usp=sharing">
+            {`Reading Pull Requests: Features, Fixes & Other Diviniations`}
+          </OutboundLink> 2019 (slides only)
+        </li>
+        <li>
+          <OutboundLink href="https://docs.google.com/presentation/d/1FgutMD7VdJMNMcz0dp9gw5_9CRPLnoaYQXHtJEDJyxQ/edit?usp=sharing">
+            {`Modern Page Objects`}
+          </OutboundLink> 2018 (slides only)
         </li>
       </ul>
 
