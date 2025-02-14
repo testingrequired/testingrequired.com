5d560957a0fe5d1f7d34863e99f968374bfc4b1c
Kylee Tilley <kyleetilley@gmail.com>
2025-02-14 00:49:14 -0600

    Fix nav menu display

--- src/components/nav-menu.js
+++ src/components/nav-menu.js
@@ -4,14 +4,14 @@ import styled from 'styled-components';
 import InlineList from './inline-list';
 
 const NavMenuStyle = styled(InlineList)`
-  display: none;
+  display: flex;
   justify-content: center;
   flex-wrap: wrap;
   font-size: 1.3em;
   margin: 0;
   margin-top: 0.5em;
 
-  @media (max-width: 400px) {
+  @media (max-width: 650px) {
     font-size: 1em;
   }
 `;
