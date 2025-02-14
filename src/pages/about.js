463bfb135294deff59259b923b132db972dd63c7
Kylee Tilley <kyleetilley@gmail.com>
2025-02-13 20:01:31 -0600

    More style improvements

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,8 +7,9 @@ import styled from 'styled-components';
 
 const AboutMeHeader = styled.div`
   float: left;
-  margin-right: 3em;
-  margin-bottom: 0.5em;
+  margin-right: 1em;
+  margin-bottom: 1em;
+  padding: 1em;
 
   @media (max-width: 600px) {
     float: none;
@@ -34,6 +35,25 @@ const Picture = styled.img`
   @media (max-width: 475px) {
     display: block;
     float: none;
+  }
+`;
+
+const Blurb = styled.section`
+  color: #333;
+  font-size: 1.1em;
+  letter-spacing: 0.01em;
+  text-align: justify;
+  text-justify: inter-character;
+
+  @media (max-width: 600px) {
+    letter-spacing: inherit;
+    font-size: 1.2em;
+  }
+
+  @media (max-width: 500px) {
+  }
+
+  @media (max-width: 475px) {
   }
 `;
 
@@ -87,71 +107,63 @@ export default function About() {
         </div>
       </AboutMeHeader>
 
-      <section>
-        <div
-          style={{
-            fontSize: '1.1em',
-            textJustify: 'inter-character',
-            textAlign: 'justify',
-          }}
-        >
-          <p>
-            Hi there! I'm a software engineer with over 10 years of professional
-            experience. I started as a self-taught coder, making webpages
-            (Geocities 😍) and writing small utilities to make my IT help desk
-            job easier.
-          </p>
+      <Blurb>
+        <p>
+          Hi there! I'm a software engineer with over 10 years of professional
+          experience. I started as a self-taught coder, making webpages
+          (Geocities 😍) and writing small utilities to make my IT help desk job
+          easier.
+        </p>
 
-          <p>
-            I broke into the industry as a tester at a small startup that
-            frequently deployed and demoed its product to investors. This
-            experience taught me how critical software quality is to the
-            business—especially as I observed how development practices directly
-            impacted the app's reliability. To help keep up with the rapid pace
-            of development (one-week sprints), I implemented extensive test
-            automation, which eventually allowed me to take on development work.
-            This shift gave me more control over the quality of the product.
-          </p>
+        <p>
+          I broke into the industry as a tester at a small startup that
+          frequently deployed and demoed its product to investors. This
+          experience taught me how critical software quality is to the
+          business—especially as I observed how development practices directly
+          impacted the app's reliability. To help keep up with the rapid pace of
+          development (one-week sprints), I implemented extensive test
+          automation, which eventually allowed me to take on development work.
+          This shift gave me more control over the quality of the product.
+        </p>
 
-          <p>
-            After the startup, I moved into software delivery consulting,
-            bringing my experience in both development and testing. I spent my
-            time pairing and mobbing with teams to implement application code
-            while advocating for better testing practices—asking about potential
-            test scenarios, encouraging developers to update end-to-end browser
-            tests, and generally influencing how teams thought about testing as
-            an integral part of development.
-          </p>
+        <p>
+          After the startup, I moved into software delivery consulting, bringing
+          my experience in both development and testing. I spent my time pairing
+          and mobbing with teams to implement application code while advocating
+          for better testing practices—asking about potential test scenarios,
+          encouraging developers to update end-to-end browser tests, and
+          generally influencing how teams thought about testing as an integral
+          part of development.
+        </p>
 
-          <p>
-            The more I worked as both a developer and tester, the more I saw a
-            deep connection between good implementation design and testability.
-            My "tester spidey sense" would tingle during story refinement or
-            while pairing on a feature. I had learned through experience—often
-            painful experience—how certain design decisions could create testing
-            headaches. Over time, I developed a strong intuition for spotting
-            trouble before it happened, and my technical background allowed me
-            to address it effectively. Being able to wear both hats at the same
-            time turned out to be a major superpower.
-          </p>
+        <p>
+          The more I worked as both a developer and tester, the more I saw a
+          deep connection between good implementation design and testability. My
+          "tester spidey sense" would tingle during story refinement or while
+          pairing on a feature. I had learned through experience—often painful
+          experience—how certain design decisions could create testing
+          headaches. Over time, I developed a strong intuition for spotting
+          trouble before it happened, and my technical background allowed me to
+          address it effectively. Being able to wear both hats at the same time
+          turned out to be a major superpower.
+        </p>
 
-          <p>
-            Over my career, I've worked across multiple industries, helping
-            organizations design and build greenfield applications, modernize
-            legacy systems, and establish best practices in development,
-            testing, and delivery. My expertise spans the entire software
-            lifecycle—from early product discussions to defining robust testing
-            strategies, building automation frameworks, and mentoring teams on
-            implementing scalable, testable, maintainable software.
-          </p>
+        <p>
+          Over my career, I've worked across multiple industries, helping
+          organizations design and build greenfield applications, modernize
+          legacy systems, and establish best practices in development, testing,
+          and delivery. My expertise spans the entire software lifecycle—from
+          early product discussions to defining robust testing strategies,
+          building automation frameworks, and mentoring teams on implementing
+          scalable, testable, maintainable software.
+        </p>
 
-          <p>
-            Outside of work, I love writing test tools, game development,
-            cooking, and synthesizers. I love to learn new things, so this list
-            is constantly growing.
-          </p>
-        </div>
-      </section>
+        <p>
+          Outside of work, I love writing test tools, game development, cooking,
+          and synthesizers. I love to learn new things, so this list is
+          constantly growing.
+        </p>
+      </Blurb>
     </Layout>
   );
 }
